1305b54ee2a9a07764e9d95462a6901a9703ce4d
Taylor Weeks <taylorlweeks707@gmail.com>
2021-02-23 10:05:16 -0500

    final commit

--- Assets/script.js
+++ Assets/script.js
@@ -6,19 +6,23 @@ $("#currentDay").text(moment().format("dddd, LL"));
     //24hr time clock; 9-hr work day
     //Access time- are designated blocks <, >, = the current time?
 function colorTimeBlocks() {
-    $(".user-event-input").each(function() {
-        console.log($(this).attr("id"))
+    $(".user-event-input").each(function() {    //For loop to run through each user-event class
+        console.log($(this).attr("id"))         //Logs id for each time block
         if (moment().hour() == $(this).attr("id")) {
-            $(this).addClass("present");   
+            $(this).addClass("present");        //If the current time is equal to time block id the block will turn red
         }
         else if (moment().hour() > $(this).attr("id")) {
-            $(this).addClass("past");
+            $(this).addClass("past");        //If the current time is greater than time block id the block will turn gray
         }
         else { 
             if (moment().hour() < $(this).attr("id")) {
-                $(this).addClass("future");
+                $(this).addClass("future");//If the current time is less than time block id the block will turn green  
         }
         }
+        var local = localStorage.getItem("user-input" + $(this).attr("id"));//variable set to retrieve user input on time block
+        if (local){
+            $(this).val(local); //if true there is info in the local storage of the time block it will show its value
+        } 
     })
     
 }
@@ -29,11 +33,16 @@ colorTimeBlocks();
 //Add text area to time blocks for user input
 
 //Create function for Save Button to save user input for each time block in local storage:
-$('#saveBtn').on('click', function() {
-    localStorage.setItem()
 
-
-//})
+$('.saveBtn').on('click', function() {
+    var hourTime = parseInt($(this).attr("data-hour")); 
+    if(hourTime < 6){ //if hour is less than 6 to 24 hour clock
+        hourTime += 12;
+    }
+    var textContent = $("#"+hourTime).val()
+    console.log(hourTime, textContent) //logs value in time block
+    localStorage.setItem("user-input" + hourTime, textContent)//stores text content in time block
+})
 
 
 
